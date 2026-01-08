878be0ebfa0a62bc43bb7bd084b1c6db4299eba2
pkanth779-blip <pkanth779@gmail.com>
2026-01-08 13:43:12 +0530

    Restore tmpfiles.org backend and update cache version

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE_NAME = 'youtube-downloader-v1';
+const CACHE_NAME = 'youtube-downloader-v3';
 const urlsToCache = [
+  '/',
   '/index.html',
-  '/'
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 // Install event - cache resources
@@ -45,24 +48,24 @@ self.addEventListener('fetch', (event) => {
         if (response) {
           return response;
         }
-        
+
         // Clone the request
         const fetchRequest = event.request.clone();
-        
+
         return fetch(fetchRequest).then((response) => {
           // Check if valid response
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
-          
+
           // Clone the response
           const responseToCache = response.clone();
-          
+
           caches.open(CACHE_NAME)
             .then((cache) => {
               cache.put(event.request, responseToCache);
             });
-          
+
           return response;
         });
       })
